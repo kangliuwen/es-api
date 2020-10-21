bb2debe9179ff70cd12185e9d05d0deaa2e954fa
kangliuwen <1094127610@qq.com>
2020-10-21 17:15:52 +0800

    es 配置信息

--- src/main/java/com/klwes/esapi/config/ElasticSeracrConfig.java
+++ src/main/java/com/klwes/esapi/config/ElasticSeracrConfig.java
@@ -7,22 +7,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- *
- *
- *@description:
- *@author: klw
- *@time: 2020-10-10 17:17
- *
+ * @description:
+ * @author: klw
+ * @time: 2020-10-10 17:17
  */
 @Configuration
 public class ElasticSeracrConfig {
     @Bean
-    public RestHighLevelClient restHighLevelClient(){
+    public RestHighLevelClient restHighLevelClient() {
         RestHighLevelClient client = new RestHighLevelClient(
                 RestClient.builder(
                         new HttpHost("127.0.0.1", 9200, "http"),
                         new HttpHost("127.0.0.1", 9300, "http")
-                        ));
+                ));
         return client;
     }
 
