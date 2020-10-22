9325abe794bab9c5322fae8cb17a860c8f00e9af
kangliuwen <1094127610@qq.com>
2020-10-22 10:14:22 +0800

    sdsdds

--- src/main/java/com/klwes/esapi/EsApiApplication.java
+++ src/main/java/com/klwes/esapi/EsApiApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EsApiApplication {
 
+
+    //dsdds
     public static void main(String[] args) {
         SpringApplication.run(EsApiApplication.class, args);
     }
